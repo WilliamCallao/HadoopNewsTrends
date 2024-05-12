c4e4fb1b0782c14fe2caee206b46929599b542d0
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-05-12 15:01:14 -0400

    React + Tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">Hola</h1>
+    </div>
+  );
+}
+
+export default App;
