38e7a337c74499c85895b4d7635b5b6e45d7660e
devferx <ferkki@hotmail.com>
2024-05-15 21:12:07 -0400

    Add filters html layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,59 @@
+import { useState } from 'react'
+
+const wordList = new Array(20).fill('').map((_, idx) => `Palabra #${idx + 1}`)
+
 export const App = () => {
+  const [words, setWords] = useState(wordList)
+  const [selectedWord, setSelectedWord] = useState('')
+
+  const onChangeSelectedWord = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedWord(e.target.id)
+  }
+
   return (
-    <main>
-      <h1>My App</h1>
+    <main className="container mx-auto min-h-screen ">
+      <header>
+        <form className="flex items-center gap-5">
+          <div className="flex flex-col">
+            <label htmlFor="">Fecha Inicial</label>
+            <input type="date" />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="">Fecha Final</label>
+            <input type="date" />
+          </div>
+
+          <input type="submit" value="Enviar" />
+        </form>
+      </header>
+      <section className="mt-5 grid grid-cols-[300px_1fr]">
+        <aside>
+          <ul className="grid gap-1">
+            {words.map((word) => (
+              <li key={word}>
+                <label className="flex items-center gap-3">
+                  <input
+                    id={`word-${word}`}
+                    type="radio"
+                    onChange={onChangeSelectedWord}
+                    checked={selectedWord === `word-${word}`}
+                  />
+                  <span className="cursor-pointer">{word}</span>
+                </label>
+              </li>
+            ))}
+          </ul>
+
+          <button
+            onClick={() => {
+              setWords([...words, `Palabra #${words.length + 1}`])
+            }}
+          >
+            Buscar Palabra
+          </button>
+        </aside>
+        <div></div>
+      </section>
     </main>
   )
 }
